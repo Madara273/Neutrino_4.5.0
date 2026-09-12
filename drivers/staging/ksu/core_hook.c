pa-xe/kernel_xiaomi_sm6375

drivers: staging: ksu: add umount calls
Change-Id: 51dc1a588c5dd6c4ae9dc3564c33930343afb6ad
Signed-off-by: TogoFire <togofire@mailfence.com>

drivers/staging/ksu/core_hook.c
@@ -1115,6 +1115,12 @@ out_ksu_try_umount:
 
 	// try umount hosts file
 	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH);
+
+	// try umount misc configs
+	ksu_try_umountt("/system/etc", false, MNT_DETACH);
+	ksu_try_umount("/product/etc", false, MNT_DETACH);
+	ksu_try_umount("/system/etc/sysconfig", false, MNT_DETACH);
+	ksu_try_umount("/product/etc/sysconfig", false, MNT_DETACH);
 #endif
 
 	return 0;

Madara273/Neutrino_4.5.0

(cherry picked from commit 78b2ad30a465e9ca64cd7278127dc06fd665c0d3)
Change-Id: Ifadff001040bd9b54eedecd4c1e82c485f0595b0
Signed-off-by: Madara273 <ravenhoxs@gmail.com>

drivers/staging/ksu/core_hook.c
@@ -1115,6 +1115,12 @@ out_ksu_try_umount:
 
 	// try umount hosts file
 	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH);
+
+	// try umount misc configs
+	ksu_try_umount("/system/etc", false, MNT_DETACH);
+	ksu_try_umount("/product/etc", false, MNT_DETACH);
+	ksu_try_umount("/system/etc/sysconfig", false, MNT_DETACH);
+	ksu_try_umount("/product/etc/sysconfig", false, MNT_DETACH);
 #endif
 
 	return 0;